176bc834ccac7d177ade61fedf502282c5809376
Ilia Bayanov <i.bajanov@itorum.ru>
2023-07-30 00:43:20 +0300

    feat: config environment

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const PORT = parseInt(process.env.PORT, 10) || 4000;
+  await app.listen(PORT);
 }
 bootstrap();
