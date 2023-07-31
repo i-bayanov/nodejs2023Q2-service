267e2b52f173805410996869cf6b7cc343c9a21d
Ilia Bayanov <i.bajanov@itorum.ru>
2023-07-31 22:41:32 +0300

    refactor: remove unused app.controller & app.service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
 import { TrackModule } from './track/track.module';
@@ -11,7 +9,5 @@ import { FavsModule } from './favs/favs.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), UserModule, ArtistModule, TrackModule, AlbumModule, FavsModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
