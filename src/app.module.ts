8b18c71054cbcec22955f8d55c7a7ad6c1fe27cf
Ilia Bayanov <i.bajanov@itorum.ru>
2023-08-12 20:36:50 +0300

    feat: add AppController & AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,12 @@ import { ArtistModule } from './artist/artist.module';
 import { TrackModule } from './track/track.module';
 import { AlbumModule } from './album/album.module';
 import { FavsModule } from './favs/favs.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [ConfigModule.forRoot(), UserModule, ArtistModule, TrackModule, AlbumModule, FavsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
