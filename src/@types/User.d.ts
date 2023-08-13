aec30f6004df5bf8dd9700ca127611feb64bc020
Ilia Bayanov <i.bajanov@itorum.ru>
2023-08-13 17:55:25 +0300

    feat: rework user module to interact with database

--- src/@types/User.d.ts
+++ src/@types/User.d.ts
@@ -1,10 +1,10 @@
-interface User {
+interface IUser {
   id: string; // uuid v4
   login: string;
   password: string;
   version: number; // integer number, increments on update
-  createdAt: number; // timestamp of creation
-  updatedAt: number; // timestamp of last update
+  createdAt: string; // timestamp of creation
+  updatedAt: string; // timestamp of last update
 }
 
 type UserWoPassword = Omit<User, 'password'>;
