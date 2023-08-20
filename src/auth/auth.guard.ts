a6300a29e17cdebcf55a7dfebf7b759ec2aec88c
Ilia Bayanov <i.p.bayanov@gmail.com>
2023-08-21 00:11:31 +0300

    feat: implement Public decorator

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,10 +1,13 @@
+import { Reflector } from '@nestjs/core';
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
 
+import { IS_PUBLIC_KEY } from 'src/decorators/public.decorator';
+
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(private jwtService: JwtService, private reflector: Reflector) {}
 
   private extractTokenFromRequest(request: Request): string | undefined {
     const [type, token] = request.headers.authorization?.split(' ') || [];
@@ -13,6 +16,12 @@ export class AuthGuard implements CanActivate {
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) return true;
+
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromRequest(request);
 
